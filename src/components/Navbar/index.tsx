f704be04747717a270bd3e0525a5b5d96463f2ce
itsme.9xx <hoanghai121298.dhtl@gmail.com>
2023-01-28 22:42:15 +0700

    update2

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,8 +1,6 @@
 const Navbar: React.FC = () => {
   return (
     <div>
-      <span className="material-symbols-outlined">light_mode</span>
-      <span className="material-symbols-outlined">exposure_plus_1</span>
       <i className="fa-duotone fa-moon-stars"></i>
       <i className="fa-sharp fa-solid fa-moon-cloud"></i>
     </div>
